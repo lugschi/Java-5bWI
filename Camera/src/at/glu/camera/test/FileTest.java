32d2b64bbe4897aa077167a9e456acd3d849ffb7
Luca Grabherr <luca.grabherr@student.htldornbirn.at>
2024-11-07 10:52:13 +0100

    FileTest geschrieben, gitignore überarbeitet

--- Camera/src/at/glu/camera/test/FileTest.java
+++ Camera/src/at/glu/camera/test/FileTest.java
@@ -1,11 +1,18 @@
 package at.glu.camera.test;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import at.glu.camera.File;
 
 public class FileTest {
     @Test
     void testGetDate() {
-        
+        File file = new File("image.png", "2024-11-07", 6);
+
+        String date = file.getDate();
+
+        Assertions.assertEquals("2024-11-07", date, "Sollte das gleiche Datum sein.");
     }
 
     @Test
